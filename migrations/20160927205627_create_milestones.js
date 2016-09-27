c6cd13dc8f73ef120d0d584de0c4df9c40b7c7e7
John Chow <JChow417@gmail.com>
2016-09-27 22:08:27 +0000

    fixed small syntax error

--- migrations/20160927205627_create_milestones.js
+++ migrations/20160927205627_create_milestones.js
@@ -2,17 +2,17 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('milestones', function(table){
-      table.increments(),
-      table.string('description'),
-      table.date('date_achieved')
+      table.increments();
+      table.string('description');
+      table.date('date_achieved');
     })
-  ])
+  ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
     knex.schema.dropTable('milestones')
-  ])
+  ]);
 };
 
 
--- migrations/20160927211456_create_milestones_insert_foreign_key.js
+++ migrations/20160927211456_create_milestones_insert_foreign_key.js
@@ -2,17 +2,17 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('milestones', function(table){
-      table.integer('famous_person_id').references('id').inTable('famous_people')
+      table.integer('famous_person_id').references('id').inTable('famous_people');
     })
-  ])
+  ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('milestones', function(table){
-      table.dropColumn('famous_person_id')
+      table.dropColumn('famous_person_id');
     })
-  ])
+  ]);
 
 };
 
